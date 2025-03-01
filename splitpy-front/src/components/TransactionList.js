90baa1d794f8ea7d2b6373e379d90a4cb0640167
Rafael <r_albuq@live.concordia.ca>
2025-03-01 11:38:05 -0500

    Transaction colapse implemented
    Reducing list density by collapsing items

--- splitpy-front/src/components/TransactionList.js
+++ splitpy-front/src/components/TransactionList.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import TransactionModal from "./TransactionModal"; // This is the modal we discussed earlier
 import "./TransactionList.css";
+import ExpandItem from "../assets/expand-height-line.svg"
+import RetractItem from "../assets/collapse-vertical-line.svg"
 
 const TransactionList = ({ groupId }) => {
   const [transactions, setTransactions] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [editingTransaction, setEditingTransaction] = useState(null);
+  const [openTransactions, setOpenTransactions] = useState({}); // Example: {1: true, 2: false, 3: true}
 
 
   // Fetch transactions for the selected group
@@ -28,6 +31,13 @@ const TransactionList = ({ groupId }) => {
       }
     };
 
+
+    const toggleTransaction = (id) => {
+    setOpenTransactions((prev) => ({
+      ...prev,
+      [id]: !prev[id], // Flip the open/close state for this specific item
+    }));
+    };
   // Handle Delete Transaction
   const handleDelTransaction = async (transactionId) => {
     const isConfirmed = window.confirm("Are you sure you want to delete this transaction?");
@@ -127,35 +137,43 @@ const onEditTransaction = async (transactionId, updatedTransaction) => {
         {transactions.map((transaction) => (
           <div key={transaction.id} >
             <div className="transaction-item">
-              <div className="mini-transaction-header">
-                <p><strong>{transaction.description}</strong></p>
-                <p><strong>Total:</strong> ${transaction.price}</p>
-                <p><strong>Date:</strong> {transaction.created_at}</p>
-              </div>
-              <ul className="member-mini-list">
-                {transaction.members.map((member, index) => (
+              <p><strong>{transaction.description}</strong></p>
+              <div className="collapsable-item" style={{ display: openTransactions[transaction.id] ? "block" : "none" }}>
+                <div className="mini-transaction-header">
+                  <p><strong>Total:</strong> ${transaction.price}</p>
+                  <p><strong>Date:</strong> {transaction.created_at}</p>
+                </div>
+                <ul className="member-mini-list">
+                  {transaction.members.map((member, index) => (
 
-                 <li key={index}>
-                  <span>
-                    <strong>{member.name} </strong> <br />
-                    <strong>Paid: </strong> ${member.paid.toFixed(2)} <br />
-                    <strong>Consumed: </strong> ${member.consumed.toFixed(2)}
-                  </span>
-                 </li>
-                ))}
-              </ul>
+                   <li key={index} className="transaction-member-info">
+                     <span>
+                       <strong>{member.name} </strong> <br />
+                       <strong>Paid: </strong> ${member.paid.toFixed(2)} <br />
+                       <strong>Consumed: </strong> ${member.consumed.toFixed(2)}
+                     </span>
+                   </li>
+                  ))}
+                </ul>
 
-              <div className="transaction-buttons">
-                <button className="delete-transaction-button"
-                  onClick={() => handleDelTransaction(transaction.id)}>
-                  Delete
+                <div className="transaction-buttons">
+                  <button className="delete-transaction-button"
+                    onClick={() => handleDelTransaction(transaction.id)}>
+                    Delete
                   </button>
 
                   <button className="edit-transaction-button"
-                  onClick={() => handleEditTransaction(transaction.id)}>
-                  Edit
-                </button>
+                    onClick={() => handleEditTransaction(transaction.id)}>
+                    Edit
+                  </button>
+                </div>
               </div>
+              <button className="itemColapse-toggle" onClick={() => toggleTransaction(transaction.id)} >
+                  <img
+                    src={openTransactions[transaction.id] ? RetractItem : ExpandItem}
+                    alt={openTransactions[transaction.id] ? "collapse" : "expand"}
+                  />
+              </button>
             </div>
           </div>
         ))}
